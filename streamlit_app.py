7ebbcdcbb236b4aeef257b25aaf3f31e4797af74
mehapant <123727932+mehapant@users.noreply.github.com>
2023-01-27 15:48:10 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 and blueberry oatmeal')
-streamlit.text('Peanut Onion Poha')
-streamlit.text('Wheat base white pasta')
-streamlit.text('Roasted Semolina khichdi')
+streamlit.header('Breakfast Favorites')
+streamlit.text('🍲Omega 3 and blueberry oatmeal')
+streamlit.text('🍹Kale, Spinach and Rocket Smoothie')
+streamlit.text('🥚Hard Boiled free range Eggs')
+streamlit.text('🥑Avocado Toast')
