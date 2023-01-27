32df66e32c33f31f79fadac89e7db62c7ed956ed
mehapant <123727932+mehapant@users.noreply.github.com>
2023-01-27 17:46:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,8 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-#Let's put a pick list for Users to select their fruits and #Let's filter the list based on the user selection
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+#Let's put a pick list for Users to select their fruits and Let's filter the list based on the user selection
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Banana', 'Orange'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 #Display the table on the screen
