0430a59fc91be732d181af9f9ecdf013025cd74b
mehapant <123727932+mehapant@users.noreply.github.com>
2023-01-30 10:06:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ streamlit.dataframe(fruits_to_show)
   #streamlit.error()
   
 #Replacing the above code with function to make it more readable and reusable
-def get_fruityvice_data(this_fruit_choice)
+def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
