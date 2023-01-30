fc725b01061e158c04d11e9b660be4c845800e1a
mehapant <123727932+mehapant@users.noreply.github.com>
2023-01-30 09:32:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,6 +73,3 @@ streamlit.dataframe(my_data_row)
 #Allow the user to add a fruit of choice
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('The user entered ', add_my_fruit)
-
-#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-#streamlit.write('The user entered ', fruit_choice)
